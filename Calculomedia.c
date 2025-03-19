23e5c9ce3e87d304e4f634634accc36ae995872d
vicentebylight <vicentebylight@gmail.com>
2025-03-19 21:20:57 +0000

    calculoint

---
+++ Calculomedia.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () {
+
+int nota1, nota2 ,nota3;
+int media;
+
+printf ("Programa de Calculo da média da classe\n");
+
+printf ("Aluno 01, digite a sua nota: \n");
+scanf ("%d", nota1);
+
+printf ("Aluno 02, digite a sua nota: \n");
+scanf ("%d", nota2);
+
+printf ("Aluno 03, digite a sua nota: \n");
+scanf ("%d", nota3);
+
+media = (nota1 + nota2 + nota3) / 3;
+
+printf ("A média da classe é: %d");
+
+
+
+
+
+
+
+
+
+
+
+
+}
